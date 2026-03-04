e03926034e5cfda853bfadeb0b6c7401ca418e6f
Aryan-Sejwal <aryansejwal2020@gmail.com>
2026-03-04 23:47:15 +0530

    updates

--- 100 days code/Day_21.c
+++ 100 days code/Day_21.c
@@ -1,52 +1,37 @@
+#include <stdio.h>
 #include <stdlib.h>
+struct Node {
+    int data;
+    struct Node *next;
+};
 
-int compare(const void* a, const void* b) {
-    return (*(int*)a - *(int*)b);
-}
+int main() {
+    int n, i;
 
-int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
-    
-    *returnSize = 0;
-    if(numsSize < 3) return NULL;
+    struct Node *head = NULL;
+    struct Node *newNode, *temp;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) {
 
-    qsort(nums, numsSize, sizeof(int), compare);
-    int** result = (int**)malloc(numsSize * numsSize * sizeof(int*));
-    *returnColumnSizes = (int*)malloc(numsSize * numsSize * sizeof(int));
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
 
-    for(int i = 0; i < numsSize - 2; i++) {
-        
-        if(i > 0 && nums[i] == nums[i - 1])
-            continue;
-
-        int left = i + 1;
-        int right = numsSize - 1;
-
-        while(left < right) {
-
-            int sum = nums[i] + nums[left] + nums[right];
-
-            if(sum == 0) {
-                result[*returnSize] = (int*)malloc(3 * sizeof(int));
-                result[*returnSize][0] = nums[i];
-                result[*returnSize][1] = nums[left];
-                result[*returnSize][2] = nums[right];
-
-                (*returnColumnSizes)[*returnSize] = 3;
-                (*returnSize)++;
-                while(left < right && nums[left] == nums[left + 1]) left++;
-                while(left < right && nums[right] == nums[right - 1]) right--;
-
-                left++;
-                right--;
+        if(head == NULL) {
+            head = newNode;
+        } else {
+            temp = head;
+            while(temp->next != NULL) {
+                temp = temp->next;
             }
-            else if(sum < 0) {
-                left++;
-            }
-            else {
-                right--;
-            }
+            temp->next = newNode;
         }
     }
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
 
-    return result;
+    return 0;
 }
