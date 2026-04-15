3b1d6c43b87396cb18058f31ff9176198857e3a6
Aryan-Sejwal <aryansejwal2020@gmail.com>
2026-04-15 23:59:23 +0530

    code

--- 100 days code/Day_73.c
+++ 100 days code/Day_73.c
@@ -2,50 +2,23 @@
 #include <string.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
+    char s[1000];
+    scanf("%s", s);
 
-    char names[n][50];     
-    int count[n];          
-    int unique = 0;        
+    int freq[26] = {0};
 
-    for (int i = 0; i < n; i++) {
-        char temp[50];
-        scanf("%s", temp);
+    for (int i = 0; s[i] != '\0'; i++) {
+        freq[s[i] - 'a']++;
+    }
 
-        int found = 0;
-
-        for (int j = 0; j < unique; j++) {
-            if (strcmp(names[j], temp) == 0) {
-                count[j]++;
-                found = 1;
-                break;
-            }
-        }
-
-        if (!found) {
-            strcpy(names[unique], temp);
-            count[unique] = 1;
-            unique++;
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (freq[s[i] - 'a'] == 1) {
+            printf("%c\n", s[i]);
+            return 0;
         }
     }
 
-    int maxVotes = 0;
-    char winner[50];
-
-    for (int i = 0; i < unique; i++) {
-        if (count[i] > maxVotes) {
-            maxVotes = count[i];
-            strcpy(winner, names[i]);
-        }
-        else if (count[i] == maxVotes) {
-            if (strcmp(names[i], winner) < 0) {
-                strcpy(winner, names[i]);
-            }
-        }
-    }
-
-    printf("%s %d\n", winner, maxVotes);
+    printf("$\n");
 
     return 0;
 }
